8123b25ba9ef58f4ead422def59abcb5be7fd322
Nigel Eke <nigeleke@gmail.com>
2024-10-19 14:15:09 +0100

    Add version to footer

--- src/components/ui/scopa_footer.rs
+++ src/components/ui/scopa_footer.rs
@@ -2,7 +2,12 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn ScopaFooter() -> Element {
+    let version = env!("CARGO_PKG_VERSION");
+    
     rsx! {
-        p { "Copyright © 2024; Nigel Eke. All rights reserved." }
+        p {
+            "Copyright © 2024; Nigel Eke. All rights reserved. ",
+            "v" {version}
+        }
     }
 }
