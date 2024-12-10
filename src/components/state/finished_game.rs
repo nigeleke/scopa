8303eb70c21e70b71afbe75d3cb5338420402e79
Nigel Eke <nigeleke@gmail.com>
2024-12-10 13:30:36 +1100

    Amend restart options to all or nothing, rather than specifics

--- src/components/state/finished_game.rs
+++ src/components/state/finished_game.rs
@@ -4,47 +4,35 @@ use crate::domain::prelude::*;
 use dioxus::prelude::*;
 
 #[component]
-pub fn FinishedGame(
-    state: FinishedState,
-    onchange: EventHandler<GameState>,
-) -> Element {
+pub fn FinishedGame(state: FinishedState, onchange: EventHandler<GameState>) -> Element {
     let winner = state.winner();
 
     let mut teams = Vec::from(state.teams());
     teams.sort_by_key(|b| std::cmp::Reverse(state.points(b.id())));
 
-    let team_points = teams.into_iter()
-        .map(|team: Team| rsx!{ TeamPoints { state: state.clone(), team: team } })
+    let team_points = teams
+        .into_iter()
+        .map(|team: Team| rsx! { TeamPoints { state: state.clone(), team: team } })
         .collect::<Vec<_>>();
 
-    let mut retain_players = use_signal(|| true);
+    let mut retain_settings = use_signal(|| true);
 
-    let update_retain_players = move |value: String| {
-        retain_players.set(str::parse::<bool>(&value).unwrap());
-    };
-
-    let mut retain_target = use_signal(|| true);
-
-    let update_retain_target = move |value: String| {
-        retain_target.set(str::parse::<bool>(&value).unwrap());
+    let update_retain_settings = move |value: String| {
+        retain_settings.set(str::parse::<bool>(&value).unwrap());
     };
 
     let start_new_game = move || {
         let mut new_game = Game::default();
 
-        if retain_players() {
+        if retain_settings() {
             let teams = Vec::from(state.teams());
             teams.iter().for_each(|t| {
                 let team = Team::new(&t.name());
-                let _ = new_game.add_team(team); } 
-            );
+                let _ = new_game.add_team(team);
+            });
         }
 
-        if retain_target() {
-            new_game.set_target(state.target());
-        }
-
-        if retain_players() && retain_target() {
+        if retain_settings() {
             onchange.call(new_game.start().unwrap());
         } else {
             onchange.call(GameState::Starting(new_game));
@@ -79,23 +67,12 @@ pub fn FinishedGame(
                         class: "finished-game-retain-checkbox",
                         Input {
                             typ: "checkbox",
-                            value: retain_players(),
-                            checked: retain_players(),
-                            on_input: update_retain_players,
-                            aria_label: "Check to use same players in new game",
+                            value: retain_settings(),
+                            checked: retain_settings(),
+                            on_input: update_retain_settings,
+                            aria_label: "Select to use same settings in new game",
                         }
-                        { " Same players" }    
-                    }
-                    div {
-                        class: "finished-game-retain-checkbox",
-                        Input {
-                            typ: "checkbox",
-                            value: retain_target(),
-                            checked: retain_target(),
-                            on_input: update_retain_target,
-                            aria_label: "Check to use same target in new game",
-                        }
-                        { " Same target" }    
+                        { " Same settings" }
                     }
                 }
             }
@@ -104,13 +81,10 @@ pub fn FinishedGame(
 }
 
 #[component]
-fn TeamPoints(
-    state: FinishedState,
-    team: Team,
-) -> Element {
+fn TeamPoints(state: FinishedState, team: Team) -> Element {
     let name = team.name();
     let points = state.points(team.id());
-    rsx ! {
+    rsx! {
         div {
             class: "finished-game-team-score",
             span { {name} }
