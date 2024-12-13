d7c41f18db162e16578cbea8d352eb48a07a6b41
Nigel Eke <nigeleke@gmail.com>
2024-12-13 11:24:37 +1100

    Use base language when dialect/region not supported

--- src/i18n/mod.rs
+++ src/i18n/mod.rs
@@ -4,9 +4,14 @@ use dioxus_i18n::{prelude::*, *};
 use unic_langid::{langid, LanguageIdentifier};
 
 pub fn config(user_langid: &str) -> I18nConfig {
-    let default_langid = LanguageIdentifier::from_str(user_langid)
+    let mut default_langid = LanguageIdentifier::from_str(user_langid)
         .ok()
         .unwrap_or(langid!("en-GB"));
+
+    let supported_dialects = [langid!("en-GB"), langid!("it-IT")];
+    if !supported_dialects.contains(&default_langid) {
+        default_langid = LanguageIdentifier::from_parts(default_langid.language, None, None, &[]);
+    }
 
     I18nConfig::new(default_langid)
         .with_locale(Locale::new_static(
@@ -15,6 +20,14 @@ pub fn config(user_langid: &str) -> I18nConfig {
         ))
         .with_locale(Locale::new_static(
             langid!("it-IT"),
+            include_str!("./it-IT.ftl"),
+        ))
+        .with_locale(Locale::new_static(
+            langid!("en"),
+            include_str!("./en-GB.ftl"),
+        ))
+        .with_locale(Locale::new_static(
+            langid!("it"),
             include_str!("./it-IT.ftl"),
         ))
         .with_fallback(langid!("en-GB"))
