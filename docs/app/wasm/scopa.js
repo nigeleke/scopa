dab74712f213edd5d468e9d25d0a201e3bd4183f
Nigel Eke <nigeleke@users.noreply.github.com>
2024-12-21 04:14:32 +0000

    Release scopa@0.1.63

--- docs/app/wasm/scopa.js
+++ docs/app/wasm/scopa.js
@@ -1394,19 +1394,19 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper2115 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2120 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 193, __wbg_adapter_48);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2128 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2133 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 193, __wbg_adapter_51);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2162 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2168 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 275, __wbg_adapter_48);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2300 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2307 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 193, __wbg_adapter_56);
         return addHeapObject(ret);
     };
