e1d33a76dbb2021cb91111b6df043abf4962eba5
Nigel Eke <nigeleke@gmail.com>
2025-12-20 20:18:45 +1100

    Make icon images 8-bit depth

--- icons/mk_icons.sh
+++ icons/mk_icons.sh
@@ -9,15 +9,15 @@ magick "$SRC" \
 TMP="icon.iconset"
 mkdir -p "$TMP"
 
-magick "$SRC" -resize 16x16   "$TMP/16x16.png"
-magick "$SRC" -resize 32x32   "$TMP/16x16@2x.png"
-magick "$SRC" -resize 32x32   "$TMP/32x32.png"
-magick "$SRC" -resize 64x64   "$TMP/32x32@2x.png"
-magick "$SRC" -resize 128x128 "$TMP/128x128.png"
-magick "$SRC" -resize 256x256 "$TMP/128x128@2x.png"
-magick "$SRC" -resize 256x256 "$TMP/256x256.png"
-magick "$SRC" -resize 512x512 "$TMP/256x256@2x.png"
-magick "$SRC" -resize 512x512 "$TMP/512x512.png"
+magick "$SRC" -resize 16x16   -strip -define png:color-type=6 -define png:bit-depth=8 "$TMP/16x16.png"
+magick "$SRC" -resize 32x32   -strip -define png:color-type=6 -define png:bit-depth=8 "$TMP/16x16@2x.png"
+magick "$SRC" -resize 32x32   -strip -define png:color-type=6 -define png:bit-depth=8 "$TMP/32x32.png"
+magick "$SRC" -resize 64x64   -strip -define png:color-type=6 -define png:bit-depth=8 "$TMP/32x32@2x.png"
+magick "$SRC" -resize 128x128 -strip -define png:color-type=6 -define png:bit-depth=8 "$TMP/128x128.png"
+magick "$SRC" -resize 256x256 -strip -define png:color-type=6 -define png:bit-depth=8 "$TMP/128x128@2x.png"
+magick "$SRC" -resize 256x256 -strip -define png:color-type=6 -define png:bit-depth=8 "$TMP/256x256.png"
+magick "$SRC" -resize 512x512 -strip -define png:color-type=6 -define png:bit-depth=8 "$TMP/256x256@2x.png"
+magick "$SRC" -resize 512x512 -strip -define png:color-type=6 -define png:bit-depth=8 "$TMP/512x512.png"
 
 magick "$TMP/*x*.png" icon.icns
 cp $TMP/*x*.png .
