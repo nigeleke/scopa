24735abb10174ddb4f8252b0c90223cd4130cb22
Nigel Eke <nigeleke@gmail.com>
2025-12-20 19:01:58 +1100

    Add windows and macos icons

---
+++ icons/mk_icons.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SRC=../design/images/base_denari.svg
+
+magick "$SRC" \
+  -define icon:auto-resize=256,128,64,48,32,16 \
+  "icon.ico"
+
+TMP="icon.iconset"
+mkdir -p "$TMP"
+
+magick "$SRC" -resize 16x16   "$TMP/16x16.png"
+magick "$SRC" -resize 32x32   "$TMP/16x16@2x.png"
+magick "$SRC" -resize 32x32   "$TMP/32x32.png"
+magick "$SRC" -resize 64x64   "$TMP/32x32@2x.png"
+magick "$SRC" -resize 128x128 "$TMP/128x128.png"
+magick "$SRC" -resize 256x256 "$TMP/128x128@2x.png"
+magick "$SRC" -resize 256x256 "$TMP/256x256.png"
+magick "$SRC" -resize 512x512 "$TMP/256x256@2x.png"
+magick "$SRC" -resize 512x512 "$TMP/512x12.png"
+
+magick "$TMP/*x*.png" icon.icns
+cp $TMP/*x*.png .
+rm -rf "$TMP"
