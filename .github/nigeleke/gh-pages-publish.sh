dd3e4da91efb5b1016725c278d5dfcba97a1bf0e
Nigel Eke <nigeleke@gmail.com>
2026-04-18 12:26:17 +1000

    Release v2.0.26

--- .github/nigeleke/gh-pages-publish.sh
+++ .github/nigeleke/gh-pages-publish.sh
@@ -3,6 +3,5 @@ sed -i 's/#base_path/base_path/g' Dioxus.toml
 cargo install cargo-binstall
 cargo binstall dioxus-cli
 dx bundle --release --platform=web --debug-symbols=false
-ls -al
-mkdir -p _site/app
-mv target/dx/*/release/web/public _site/app
+mkdir -p site/app
+mv target/dx/*/release/web/public site/app
